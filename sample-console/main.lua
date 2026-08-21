c22545fa3b8e6b8029472110f1799ef6b8280991
Anthony Mortimer <agmortimer20@gmail.com>
2026-08-20 22:44:16 -0400

    Publish configs and starter code

---
+++ sample-console/main.lua
@@ -0,0 +1,4 @@
+print("Hello!")
+io.write("Enter your name: ")
+local name = io.read()
+print("Nice to meet you " .. name)
--- sample-game/main.lua
+++ sample-game/main.lua
@@ -1,7 +1,3 @@
-function love.load()
-    print("Hello from the terminal")
-end
-
 function love.draw()
     love.graphics.print('Hello World!', 400, 300)
 end
